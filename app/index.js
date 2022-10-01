badffb12b3c9da023b26c9991b6c9d30d9b54ed9
Sebas <SebasYs@protonmail.com>
2022-10-01 12:15:23 -0400

    ya casi

---
+++ app/index.js
@@ -0,0 +1,25 @@
+const buttons = document.querySelectorAll(".bnt");
+const submitButton = document.querySelector('.sub_bnt');
+const thankYouState = document.querySelector('.thankyou_state');
+const ratingState = document.querySelector('.rating_state');
+const ratingValue = document.querySelector('.rating_value');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        ratingValue.append = button.innerHTML;
+    })
+})
+
+
+
+submitButton.addEventListener('click', e =>{
+    ratingState.style.display = 'none';
+    thankYouState.style.display= 'flex';
+
+    console.log('si weeee ')
+})
+
+
+
+
+console.log(ratingValue)
